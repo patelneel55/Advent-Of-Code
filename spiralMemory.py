e11325f1f91c613ea447496b58ad276b47e4f923
patelneel55 <patelneel55@gmail.com>
2017-12-03 20:39:07 -0500

    part2 - algorithm of the cell which is equal to the sum of its adjacent cells (spiral)

--- spiralMemory.py
+++ spiralMemory.py
@@ -1,7 +1,10 @@
+from itertools import count
+
 import math
 def main():
 	num = input("Enter a number: ")
 	print "Part 1: ", part1(num)
+	print "Part 2: ", part2(num)
 
 def part1(num):
 	i = 0
@@ -16,6 +19,22 @@ def part1(num):
 	else:
 		avg = (2 * sq - 3 * i + 3) / 2
 		return (i / 2) + abs(num - avg)
+
+
+def sum_spiral():
+    a, i, j = {(0,0) : 1}, 0, 0
+    sn = lambda i,j: sum(a.get((k,l), 0) for k in range(i-1,i+2)
+                                         for l in range(j-1,j+2))
+    for s in count(1, 2):
+        for _ in range(s):   i += 1; a[i,j] = sn(i,j); yield a[i,j]
+        for _ in range(s):   j -= 1; a[i,j] = sn(i,j); yield a[i,j]
+        for _ in range(s+1): i -= 1; a[i,j] = sn(i,j); yield a[i,j]
+        for _ in range(s+1): j += 1; a[i,j] = sn(i,j); yield a[i,j]
+
+def part2(num):
+	for i in sum_spiral():
+		if i > num:
+			return i
 
 if __name__ == "__main__":
 	main()
