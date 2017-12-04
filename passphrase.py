8e027b705c6d3f4630195eded53301b1216b35a2
patelneel55 <patelneel55@gmail.com>
2017-12-04 15:04:49 -0500

    part2 - valid passphrase counter that is valid when passphrase are not anagrams

--- passphrase.py
+++ passphrase.py
@@ -1,5 +1,6 @@
 def main():
 	print "Part 1: ", part1()
+	print "Part 2: ", part2()
 
 def part1():
 	file = open("pass.txt", "r")
@@ -16,6 +17,28 @@ def part1():
 			else:
 				flag = False
 		if flag == True:
+			num += 1
+	file.close()
+	return num
+	
+def part2():
+	file = open("pass.txt", "r")
+	list = []
+	num = 0
+	flag = True
+	for line in file:
+		flag = True
+		list = []
+		words = line.split()
+		for i in xrange(len(words)):
+			words[i] = ''.join(sorted(words[i]))
+		dup = []
+		for x in words:
+			if x not in dup:
+				dup.append(x)
+			else:
+				flag = False
+		if flag:
 			num += 1
 	file.close()
 	return num
