6f61cb07999d1116bdfba4121fabeb36fdaa460f
patelneel55 <patelneel55@gmail.com>
2017-12-05 21:41:06 -0500

    part2 - change offset incrementation and decrementation

--- twistyTramp.py
+++ twistyTramp.py
@@ -1,5 +1,6 @@
 def main():
 	print "Part 1: ", part1()
+	print "Part 2: ", part2()
 	
 def part1():
 	file = open("twisty.txt", "r")
@@ -13,6 +14,26 @@ def part1():
 		prev = i
 		i += int(list[i])
 		list[prev] = int(list[prev]) + 1
+		steps += 1
+		
+	return steps
+	
+def part2():
+	file = open("twisty.txt", "r")
+	list = file.readlines()
+	
+	steps = 0
+	i = 0
+	while 1:
+		if(i < 0 or i >= len(list)):
+			break
+		prev = i
+		i += int(list[i])
+		if int(list[prev]) >= 3:
+			list[prev] = int(list[prev]) - 1
+		else:
+			list[prev] = int(list[prev]) + 1
+		
 		steps += 1
 		
 	return steps
