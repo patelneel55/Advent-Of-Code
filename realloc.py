1c77aa612a8f35e6b04f7d4b5df3c3a71d420cc8
patelneel55 <patelneel55@gmail.com>
2017-12-06 01:57:58 -0500

    part2 - number of cycles of mem realloc between same spaces

--- realloc.py
+++ realloc.py
@@ -1,5 +1,6 @@
 def main():
-	print "Part 1:", part1()
+	print "Part 1: ", part1()
+	print "Part 2: ", part2()
 
 def part1():
 	file = open("mem.txt", "r")
@@ -43,6 +44,50 @@ def part1():
 		test.append(sForm)
 	
 	return its
+
+def part2():
+	file = open("mem.txt", "r")
+	list = file.read()
+	list = map(int, list.split())
+	its = 0
+	length = len(list)
+	mx = list[0]
+	ind = 0
+	test = []
+	sForm = ""
+	while 1:
+		mx = max(list)
+		ind = list.index(max(list))
+		i = 0
+		if mx >= (length - 1):
+
+			while (mx - i) % (length-1) != 0:
+				i += 1
+
+			mx = (mx - i) / (length - 1)
+			for m in xrange(length):
+				if m != ind:
+					list[m] += mx
+			list[ind] = i
+		else:
+			count = 0
+			for k in xrange(ind+1, length):
+				if count < mx:
+					list[k] += 1
+					count += 1
+			for a in xrange(0, ind + 1):
+				if count < mx:
+					count += 1
+					list[a] += 1
+			list[ind] = 0
+
+		its += 1
+		sForm = ''.join(map(str, list))
+		if(test.count(sForm) > 0):
+			break;
+		test.append(sForm)
+	num = len(test) - test.index(sForm)
+	return num
 
 
 if __name__ == "__main__":
