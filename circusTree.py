71c03fe593649eca2e7861a992f77b50595ab595
patelneel55 <patelneel55@gmail.com>
2017-12-07 01:11:36 -0500

    part1 - create a tree of parent and sub-parents with childs until only one parent exists

---
+++ circusTree.py
@@ -0,0 +1,41 @@
+def main():
+	print "Part 1: ", part1()
+
+def part1():
+	f = open("day7.txt", "r")
+	treeDict = {}
+
+	for line in f:
+
+		# Splits the string into parent and child list
+		tree = line.split(" -> ")
+		child = []
+		if len(tree) == 1:
+			continue
+
+		# Extract weight from the list
+		tree[0] = tree[0].split()[0]
+		child = tree[1].split(", ")
+		for i in xrange(len(child)):
+			child[i] = child[i].strip("\r\n")
+		tree[1] = child
+		treeDict[tree[0]] = tree[1]
+
+	toDel = []
+	while len(treeDict) != 1:
+		for k in treeDict:
+			for m in xrange(len(treeDict[k])):
+				if treeDict[k][m] in treeDict:
+					key = treeDict[k][m]
+					treeDict[k][m] = treeDict[key]
+					toDel.append(key)
+		for s in toDel:
+			del treeDict[s]
+
+	for m in treeDict:
+		return m
+
+
+
+if __name__ == "__main__":
+	main()
