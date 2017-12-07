cbee7a07af2d06e1c40ab247340d6a8434ee50a4
patelneel55 <patelneel55@gmail.com>
2017-12-07 14:03:43 -0500

    part1 - update code

--- circusTree.py
+++ circusTree.py
@@ -1,5 +1,6 @@
 def main():
 	print "Part 1: ", part1()
+	print "Part 2: ", part2()
 
 def part1():
 	f = open("day7.txt", "r")
@@ -34,6 +35,44 @@ def part1():
 
 	for m in treeDict:
 		return m
+
+def part2():
+	f = open("day7.txt", "r")
+	treeDict = {}
+	weightDict = {}
+	
+	for line in f:
+
+		# Splits the string into parent and child list
+		tree = line.split(" -> ")
+		child = []
+		if len(tree) == 1:
+			wght = tree[0].split()
+			wght[1] = wght[1].strip("()")
+			print wght
+			continue
+
+		# Extract weight from the list
+		tree[0] = tree[0].split()[0]
+		child = tree[1].split(", ")
+		for i in xrange(len(child)):
+			child[i] = child[i].strip("\r\n")
+		tree[1] = child
+		treeDict[tree[0]] = tree[1]
+
+	toDel = []
+	while len(treeDict) != 1:
+		for k in treeDict:
+			for m in xrange(len(treeDict[k])):
+				if treeDict[k][m] in treeDict:
+					key = treeDict[k][m]
+					treeDict[k][m] = treeDict[key]
+					toDel.append(key)
+		for s in toDel:
+			del treeDict[s]
+
+	print treeDict
+		
 
 
 
