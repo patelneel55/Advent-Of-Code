38b78988c7edbb404e488256a2cdc446d62f4476
patelneel55 <patelneel55@gmail.com>
2017-12-09 22:28:57 -0500

    part2 - count all garbage characters

--- process.py
+++ process.py
@@ -1,5 +1,6 @@
 def main():
 	print "Part 1: ", part1()
+	print "Part 2: ", part2()
 
 def part1():
 	f = open("day9.txt", "r")
@@ -35,6 +36,32 @@ def part1():
 		note = False
 
 	return gsum
+
+
+def part2():
+	f = open("day9.txt", "r")
+	code = f.readlines()
+	code = code[0]
+
+	charCount = 0
+	garbo = False
+	note = False
+
+	for text in code:
+		if text == '<' and not note and not garbo:
+			garbo = True
+			continue
+		if text == '>' and not note:
+			garbo = False
+		if text == '!' and garbo and not note:
+			note = True
+			continue
+
+		if garbo and not note:
+			charCount += 1
+		note = False
+
+	return charCount
 
 if __name__ == "__main__":
 	main()
