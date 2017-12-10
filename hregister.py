86e26d9704e3e267d23e16b4cddb13a51da0d3fc
patelneel55 <patelneel55@gmail.com>
2017-12-10 00:05:54 -0500

    part 1&2 - optimized solution using eval and compile

--- hregister.py
+++ hregister.py
@@ -1,72 +1,29 @@
-import operator
-
+mx = 0
 def main():
 	print "Part 1: ", part1()
+	print "Part 2: ", mx
 
 def part1():
+	global mx
 	f = open("day8.txt", "r")
 	
 	varDict = {}
-
+	
 	for line in f:
-		comp = line.strip().split(" ")
-		if comp[1] == "inc":
-			comp[1] = "+"
-		else:
-			comp[1] = "-"
+		comp = line.strip().split()
 
 		if not comp[4] in varDict:
 			varDict[comp[4]] = 0
 		if not comp[0] in varDict:
 			varDict[comp[0]] = 0
+		if comp[1] == "inc":
+			command = "varDict[comp[0]] += " + comp[2] + " if varDict[comp[4]] " + comp[5] + " " + comp[6] + " else 0"
+		else:
+			command = "varDict[comp[0]] -= " + comp[2] + " if varDict[comp[4]] " + comp[5] + " " + comp[6] + " else 0"
+		eval(compile(command, '<string>', 'exec'))
+		if mx < varDict[comp[0]]:
+			mx = varDict[comp[0]]
 
-		if comp[5] == ">=":
-			if varDict[comp[4]] >= int(comp[6]):
-				if comp[1] == "+":
-					varDict[comp[0]] += int(comp[2])
-				else:
-					varDict[comp[0]] -= int(comp[2])
-		
-
-		if comp[5] == ">":
-			if varDict[comp[4]] > int(comp[6]):
-				if comp[1] == "+":
-					varDict[comp[0]] += int(comp[2])
-				else:
-					varDict[comp[0]] -= int(comp[2])
-
-
-		if comp[5] == "<=":
-			if varDict[comp[4]] <= int(comp[6]):
-				if comp[1] == "+":
-					varDict[comp[0]] += int(comp[2])
-				else:
-					varDict[comp[0]] -= int(comp[2])
-
-		if comp[5] == "<":
-			if varDict[comp[4]] < int(comp[6]):
-				if comp[1] == "+":
-					varDict[comp[0]] += int(comp[2])
-				else:
-					varDict[comp[0]] -= int(comp[2])
-
-		if comp[5] == "==":
-			if varDict[comp[4]] == int(comp[6]):
-				if comp[1] == "+":
-					varDict[comp[0]] += int(comp[2])
-				else:
-					varDict[comp[0]] -= int(comp[2])
-
-		if comp[5] == "!=":
-			if varDict[comp[4]] != int(comp[6]):
-				if comp[1] == "+":
-					varDict[comp[0]] += int(comp[2])
-				else:
-					varDict[comp[0]] -= int(comp[2])
-
-
-	lst = sorted(varDict.values(), key=int, reverse=True)
-	return lst[0]
-
+	return max(varDict.values())	
 if __name__ == "__main__":
 	main()
