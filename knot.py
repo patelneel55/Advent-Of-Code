358f6ad967bbfd1aa095516aadbf309a9cda82e7
patelneel55 <patelneel55@gmail.com>
2017-12-10 15:33:25 -0500

    part1 - traverse list and flip to create a encryption algorithm

---
+++ knot.py
@@ -0,0 +1,38 @@
+def main():
+	print "Part 1: ", part1()
+
+def part1():
+	f = open("day10.txt", "r")
+	line = f.read()
+	line = line.strip().split(",")
+	
+	aryList = []
+	count = 0
+	skip = 0
+	for i in xrange(256):
+		aryList.append(i)
+	for num in line:
+		temp = []
+		ct = count
+		for i in xrange(int(num)):
+			temp.append(aryList[count])
+			count += 1
+			if count >= len(aryList):
+				count = 0
+		if (count+skip) >= len(aryList):
+			count = (count+skip) - len(aryList)
+		else:
+			count += skip
+		skip += 1
+		temp.reverse()
+		for i in xrange(int(num)):
+			aryList[ct] = temp[i]
+			ct += 1
+			if ct >= len(aryList):
+				ct = 0
+	
+	return aryList[0]*aryList[1]
+		
+
+if __name__ == "__main__":
+	main()
