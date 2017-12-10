399d31a67f8021e06c29b2f49d77d7642363f5cc
patelneel55 <patelneel55@gmail.com>
2017-12-09 23:15:07 -0500

    part1 - convert string mathematical commands to numbers and print highest result

--- hregister.py
+++ hregister.py
@@ -1,11 +1,72 @@
+import operator
+
 def main():
 	print "Part 1: ", part1()
-	print "Part 2: ", part2()
 
-def part1()
+def part1():
+	f = open("day8.txt", "r")
+	
+	varDict = {}
+
+	for line in f:
+		comp = line.strip().split(" ")
+		if comp[1] == "inc":
+			comp[1] = "+"
+		else:
+			comp[1] = "-"
+
+		if not comp[4] in varDict:
+			varDict[comp[4]] = 0
+		if not comp[0] in varDict:
+			varDict[comp[0]] = 0
+
+		if comp[5] == ">=":
+			if varDict[comp[4]] >= int(comp[6]):
+				if comp[1] == "+":
+					varDict[comp[0]] += int(comp[2])
+				else:
+					varDict[comp[0]] -= int(comp[2])
+		
+
+		if comp[5] == ">":
+			if varDict[comp[4]] > int(comp[6]):
+				if comp[1] == "+":
+					varDict[comp[0]] += int(comp[2])
+				else:
+					varDict[comp[0]] -= int(comp[2])
 
 
-def part2()
+		if comp[5] == "<=":
+			if varDict[comp[4]] <= int(comp[6]):
+				if comp[1] == "+":
+					varDict[comp[0]] += int(comp[2])
+				else:
+					varDict[comp[0]] -= int(comp[2])
+
+		if comp[5] == "<":
+			if varDict[comp[4]] < int(comp[6]):
+				if comp[1] == "+":
+					varDict[comp[0]] += int(comp[2])
+				else:
+					varDict[comp[0]] -= int(comp[2])
+
+		if comp[5] == "==":
+			if varDict[comp[4]] == int(comp[6]):
+				if comp[1] == "+":
+					varDict[comp[0]] += int(comp[2])
+				else:
+					varDict[comp[0]] -= int(comp[2])
+
+		if comp[5] == "!=":
+			if varDict[comp[4]] != int(comp[6]):
+				if comp[1] == "+":
+					varDict[comp[0]] += int(comp[2])
+				else:
+					varDict[comp[0]] -= int(comp[2])
+
+
+	lst = sorted(varDict.values(), key=int, reverse=True)
+	return lst[0]
 
 if __name__ == "__main__":
 	main()
