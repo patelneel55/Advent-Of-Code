c020b921787cc2ac369fa0520b268dbec7adebac
patelneel55 <patelneel55@gmail.com>
2017-12-11 00:52:39 -0500

    par1 and part2 - find hex routes from start using co-ordinate system

---
+++ hex.py
@@ -0,0 +1,32 @@
+import math
+
+def main():
+	part1()
+
+def part1():
+	f = open("day11.txt", "r")
+	line = f.readlines()
+	line = line[0].strip().split(",")
+
+	highest = 0
+	x = 0
+	y = 0
+	for command in line:
+		if command[0] == 's':
+			y -= 1
+		else:
+			y += 1
+
+		if command[-1] == 'e':
+			x += 1
+		elif command[-1] == 'w':
+			x -= 1
+		steps = max(abs(x), abs(y))
+		if steps > highest:
+			highest = steps
+
+	print "Part 1: ", steps
+	print "Part 2: ", highest
+
+if __name__ == "__main__":
+	main()
