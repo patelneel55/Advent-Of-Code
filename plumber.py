6013d5678d23dac96f845f485d9ca4ea2779e4ed
patelneel55 <patelneel55@gmail.com>
2017-12-12 17:38:52 -0500

    part2 - count total number of groups

--- plumber.py
+++ plumber.py
@@ -7,6 +7,7 @@ def main():
 		except EOFError:
 			break
 	print "Part 1: ", part1(buffer)
+	print "Part 2: ", part2(buffer)
 def part1(line):
 	zeroCon = []
 
@@ -27,6 +28,28 @@ def part1(line):
 		if num == len(zeroCon):
 			break
 	return len(zeroCon) + 1
+
+def part2(lines):	
+	adjlist = {}
+	for line in lines:
+		splitline = line.split(' <-> ')
+		dest = splitline[1].split(', ')
+		destint = [int(a) for a in dest]
+		adjlist[int(splitline[0])] = destint
+
+	counter = 0
+	found = {}
+	for key in adjlist:
+		if key not in found:
+			counter += 1
+			q =	[key]
+			while len(q) > 0:
+				a = q.pop(0)
+				if a not in found:
+					found[a] = 1
+					for elem in adjlist[a]:
+						q.append(elem)
+	return counter
 		
 	
 if __name__ == "__main__":
